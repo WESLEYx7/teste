08a30d9a1dce88962ea7db506d38cc4ede434730
brunoramalho01 <brunoramalho01@gmail.com>
2024-02-12 23:08:17 -0400

    Primeiro Upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# modelo-sys-sbadmin
---
+++ app/pages/usuarios.php
@@ -0,0 +1,27 @@
+<table id="datatablesSimple" class="display">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Função</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Usuário 1</td>
+                <td>usuario1@example.com</td>
+                <td>Administrador</td>
+            </tr>
+            <tr>
+                <td>Usuário 2</td>
+                <td>usuario2@example.com</td>
+                <td>Usuário Padrão</td>
+            </tr>
+            <tr>
+                <td>Usuário 3</td>
+                <td>usuario3@example.com</td>
+                <td>Usuário Padrão</td>
+            </tr>
+            <!-- Adicione mais linhas conforme necessário -->
+        </tbody>
+    </table>
